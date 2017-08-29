8ebf018522c3fbbbb939a8d9d085e02c87b11000
Pham Huu Binh <binhpham1909@gmail.com>
2017-08-29 14:41:36 +0700

    Tao moi project IoT

---
+++ IoTFirmware/.ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-08-29 14:40:15
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "IoTFirmware.ino"
+
+
+#endif
