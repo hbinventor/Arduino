52f5de0cae693f7989ace84361d450a9eeb42b03
Pham Huu Binh <binhpham1909@gmail.com>
2017-08-31 17:00:26 +0700

    create project on acer laptop

--- IoTFirmware/.ino.cpp
+++ IoTFirmware/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-08-30 11:51:09
+//This file has been generated on 2017-08-31 16:52:14
 
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
